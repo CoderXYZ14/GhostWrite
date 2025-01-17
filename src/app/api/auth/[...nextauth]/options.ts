d0a41cad011fc2b2200e0cf0816e12d9e6f52200
CoderXYZ14 <s1404islam@gmail.com>
2025-01-18 03:50:47 +0530

    options.ts auth page and its type modified

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,91 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+
+export const authOtions: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Email",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req): Promise<any> {
+        if (!credentials?.username || !credentials?.password) {
+          throw new Error("Email and password are required");
+        }
+
+        await dbConnect();
+        try {
+          const user = await UserModel.findOne({
+            $or: [
+              { email: credentials.username },
+              {
+                username: credentials.username,
+              },
+            ],
+          });
+
+          if (!user) {
+            throw new Error("No user found with this email");
+          }
+          if (!user.isVerified) {
+            throw new Error("Please verify your account before login");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+
+          if (isPasswordCorrect) {
+            return user;
+          } else {
+            throw new Error("Password is incorrect");
+          }
+        } catch (error) {
+          throw new Error(`Error in authorize function: ${error}`);
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, user, token }) {
+      if (token) {
+        session.user._id = token._id;
+        session.user.isVerified = token.isVerified;
+        session.user.isAcceptingMessages = token.isAcceptingMessages;
+        session.user.username = token.username;
+      }
+      return session;
+    },
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        /*
+        gives error initially as next auth dont let u take any
+        thing from user so u modify the user object types 
+        in types/next-auth.d.ts
+        */
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
+  },
+};
