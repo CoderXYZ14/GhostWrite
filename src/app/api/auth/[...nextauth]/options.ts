ff7fcda8cb4b9dbc4f3f2400d21e17b963dc3a87
CoderXYZ14 <s1404islam@gmail.com>
2025-01-26 01:04:06 +0530

    vercel deployment error

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,7 +3,6 @@ import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
 import dbConnect from "@/lib/dbConnect";
 import UserModel from "@/model/User";
-import { User } from "@/model/User";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -11,58 +10,49 @@ export const authOptions: NextAuthOptions = {
       id: "credentials",
       name: "Credentials",
       credentials: {
-        username: {
-          label: "Email",
-          type: "text",
-          placeholder: "jsmith@gmail.com",
-        },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials: any): Promise<any> {
-        if (!credentials?.username || !credentials?.password) {
-          throw new Error("Email and password are required");
-        }
-
         await dbConnect();
         try {
           const user = await UserModel.findOne({
             $or: [
-              { email: credentials.username },
-              { username: credentials.username },
+              { email: credentials.identifier },
+              { username: credentials.identifier },
             ],
           });
-
           if (!user) {
             throw new Error("No user found with this email");
           }
           if (!user.isVerified) {
-            throw new Error("Please verify your account before login");
+            throw new Error("Please verify your account before logging in");
           }
-
           const isPasswordCorrect = await bcrypt.compare(
             credentials.password,
             user.password
           );
-
           if (isPasswordCorrect) {
             return user;
           } else {
-            throw new Error("Password is incorrect");
+            throw new Error("Incorrect password");
           }
-        } catch (error) {
-          throw new Error(`Error in authorize function: ${error}`);
+        } catch (err: any) {
+          throw new Error(err);
         }
       },
     }),
   ],
-  pages: {
-    signIn: "/signin",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString(); // Convert ObjectId to string
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
     async session({ session, token }) {
       if (token) {
         session.user._id = token._id;
@@ -72,14 +62,12 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
-    async jwt({ token, user }) {
-      if (user) {
-        token._id = user._id?.toString();
-        token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessages;
-        token.username = user.username;
-      }
-      return token;
-    },
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  pages: {
+    signIn: "/signin",
   },
 };
