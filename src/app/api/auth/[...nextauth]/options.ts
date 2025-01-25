0e57524c9fcd5e02676367ec5127ba4f864f329c
CoderXYZ14 <s1404islam@gmail.com>
2025-01-26 01:13:54 +0530

    vercel deployment error

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,49 +10,59 @@ export const authOptions: NextAuthOptions = {
       id: "credentials",
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "text" },
+        username: {
+          label: "Email",
+          type: "text",
+          placeholder: "jsmith@gmail.com",
+        },
         password: { label: "Password", type: "password" },
       },
+      // @ts-ignore
       async authorize(credentials: any): Promise<any> {
+        if (!credentials?.username || !credentials?.password) {
+          throw new Error("Email and password are required");
+        }
+
         await dbConnect();
         try {
           const user = await UserModel.findOne({
             $or: [
-              { email: credentials.identifier },
-              { username: credentials.identifier },
+              { email: credentials.username },
+              { username: credentials.username },
             ],
           });
+
           if (!user) {
             throw new Error("No user found with this email");
           }
           if (!user.isVerified) {
-            throw new Error("Please verify your account before logging in");
+            throw new Error("Please verify your account before login");
           }
+
           const isPasswordCorrect = await bcrypt.compare(
             credentials.password,
             user.password
           );
+
           if (isPasswordCorrect) {
             return user;
           } else {
-            throw new Error("Incorrect password");
+            throw new Error("Password is incorrect");
           }
-        } catch (err: any) {
-          throw new Error(err);
+        } catch (error) {
+          throw new Error(`Error in authorize function: ${error}`);
         }
       },
     }),
   ],
+  pages: {
+    signIn: "/signin",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token._id = user._id?.toString(); // Convert ObjectId to string
-        token.isVerified = user.isVerified;
-        token.isAcceptingMessages = user.isAcceptingMessages;
-        token.username = user.username;
-      }
-      return token;
-    },
     async session({ session, token }) {
       if (token) {
         session.user._id = token._id;
@@ -62,12 +72,14 @@ export const authOptions: NextAuthOptions = {
       }
       return session;
     },
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-  pages: {
-    signIn: "/signin",
+    async jwt({ token, user }) {
+      if (user) {
+        token._id = user._id?.toString();
+        token.isVerified = user.isVerified;
+        token.isAcceptingMessages = user.isAcceptingMessages;
+        token.username = user.username;
+      }
+      return token;
+    },
   },
 };
