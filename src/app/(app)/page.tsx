43233d1c172c4338de1b3455877add1287fc2455
CoderXYZ14 <s1404islam@gmail.com>
2025-01-26 20:49:16 +0530

    Homepage button position changed

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
                     security.
                   </p>
                 </div>
-                <div className="flex flex-col gap-2 ">
+                <div className="flex  gap-2 flex-row">
                   <Link href="/dashboard">
                     <Button
                       size="lg"
